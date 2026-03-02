99609c4c3b029cf037f29e908b6870e0b178aa64
Naimlyss <naimlysstheonlyone@gmail.com>
2026-03-02 17:04:03 +0700

    modify : name

--- portfolio/src/i18n/messages/en.ts
+++ portfolio/src/i18n/messages/en.ts
@@ -1,11 +1,11 @@
-const en = {
+﻿const en = {
   nav: {
     home: "Home",
     projects: "Projects",
     contact: "Contact",
   },
   thurein: {
-    title: "Thurein Tun's Portfolio",
+    title: "Hein Thant Aung's Portfolio",
     description: "A full-stack developer with 1 year of hands-on experience in web and backend development. Skilled in building secure, user-friendly systems using modern technologies. Passtionate in continuous learning, solving real-world problems, contributing to teams with dedication."
   },
   actions: {
